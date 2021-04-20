6369a77939fc8875e42f9f8c450c28535f270218
GidSchwifty <gidramch@gmail.com>
2021-04-20 17:06:58 -0400

    minor formatting

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,5 +1,7 @@
 import authController from './auth/authController';
 import googleAuthController from './google-auth/googleAuthController';
+
+
 /**
  * Array of all components, to ensure app.ts remains clean
  */
